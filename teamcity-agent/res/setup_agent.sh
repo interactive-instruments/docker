ec22a9013c3d1f5ef3ab25749dd7aa609b2d2669
Jon Herrmann <herrmann@interactive-instruments.de>
2016-02-10 17:29:42 +0100

    team city agent

---
+++ teamcity-agent/res/setup_agent.sh
@@ -0,0 +1,39 @@
+#!/usr/bin/env bash
+
+AGENT_DIR="/home/teamcity/agent"
+
+FIRST_INSTALL_SLEEP_TIME=120
+
+if [ -z "$TEAMCITY_SERVER_URL" ]; then
+    echo "Error: TEAMCITY_SERVER_URL is not set."
+    echo
+    exit
+fi
+
+echo "Agent directory set to ${AGENT_DIR}."
+
+if [ ! -d "$AGENT_DIR" ]; then
+    cd ${HOME}
+    echo "Setting up Agent for the first time."
+    echo "Sleeping ${FIRST_INSTALL_SLEEP_TIME} seconds to ensure server is up."
+    sleep ${FIRST_INSTALL_SLEEP_TIME}
+    wget --waitretry=30 --retry-connrefused $TEAMCITY_SERVER_URL/update/buildAgent.zip
+    if [[ ! -f buildAgent.zip ]] ; then
+       echo 'File does not exist, aborting.'
+       exit
+    fi
+    mkdir -p $AGENT_DIR
+    unzip -q -d $AGENT_DIR buildAgent.zip
+    rm buildAgent.zip
+    chmod +x $AGENT_DIR/bin/agent.sh
+    chown teamcity:teamcity -R $AGENT_DIR
+    echo "serverUrl=${TEAMCITY_SERVER_URL}" > $AGENT_DIR/conf/buildAgent.properties
+    ## Blank, created from build agent's host name
+    echo "name=" >> $AGENT_DIR/conf/buildAgent.properties
+    echo "workDir=${AGENT_DIR}/work" >> $AGENT_DIR/conf/buildAgent.properties
+    echo "tempDir=${AGENT_DIR}/temp" >> $AGENT_DIR/conf/buildAgent.properties
+    echo "systemDir=${AGENT_DIR}/system" >> $AGENT_DIR/conf/buildAgent.properties
+    cat ${AGENT_DIR}/conf/buildAgent.properties
+fi
+sleep 2
+sudo -Hu teamcity sh -c 'export "${1}"; cd $AGENT_DIR/../ && ./agent/bin/agent.sh run' _ "AGENT_DIR=${AGENT_DIR}"
