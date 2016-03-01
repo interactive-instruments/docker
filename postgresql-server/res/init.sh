5de5fb58880728a75da8d832a4f8eaf5d00f3957
Jon Herrmann <herrmann@interactive-instruments.de>
2016-03-01 22:24:11 +0100

    user creation removed
    supported by postgresql image with sql scripts now

--- postgresql-server/res/init.sh
+++ postgresql-server/res/init.sh
@@ -2,27 +2,6 @@
 
 echo "Running custom init"
 
-if [ -n "${USERS}" ]; then
-  
-  users=($(awk -F',' '{for (i = 1 ; i <= NF ; i++) print $i}' <<< "${USERS}"));
-  passwords=($(awk -F',' '{for (i = 1 ; i <= NF ; i++) print $i}' <<< "${PASSWORDS}"));
-  schemas=($(awk -F',' '{for (i = 1 ; i <= NF ; i++) print $i}' <<< "${SCHEMAS}"));
-  
-  for i in ${!users[*]}; do
-     echo "****** CREATING USER \"${users[i]}\" and database \"${schemas[i]}\" ******"
-
-
-gosu postgres postgres --single <<- EOSQL
-   CREATE DATABASE ${schemas[i]};
-   CREATE USER ${users[i]};
-   ALTER USER ${users[i]} with password '${passwords[i]}';
-   ALTER USER ${users[i]} CREATEDB;
-   GRANT ALL PRIVILEGES ON DATABASE ${schemas[i]} to ${users[i]};
-EOSQL
-     echo ""
-     echo "******OK ******"
-  done
-fi
 
 sed -i "s/^\(shared_buffers\s*=\s*\).*$/\1$SHARED_BUFFERS/g" /var/lib/postgresql/data/postgresql.conf
 
