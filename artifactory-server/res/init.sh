54daec65657fdd6c202b23090080a6c4d39856e1
Jon Herrmann <herrmann@interactive-instruments.de>
2016-04-20 22:14:17 +0200

    fix : artifactory

--- artifactory-server/res/init.sh
+++ artifactory-server/res/init.sh
@@ -1,15 +1,26 @@
 #!/bin/bash
 
 chown -R artifactory:artifactory /opt/artifactory
+chmod 750 -R /opt/artifactory/
 
-webapp_name=${RELATIVE_URL}
+if [ -n "${RELATIVE_URL}" ]; then
+if [ ! -f /opt/artifactory/webapps/${RELATIVE_URL}.war ]; then
+echo "Changing context for relative URL ${RELATIVE_URL}"
 
-cat << EOF > "/opt/artifactory/tomcat/conf/Catalina/localhost/$webapp_name.xml"
+rm /opt/artifactory/tomcat/conf/Catalina/localhost/artifactory.xml
+
+cat << EOF > "/opt/artifactory/tomcat/conf/Catalina/localhost/${RELATIVE_URL}.xml"
 <?xml version="1.0" encoding="UTF-8"?>
-<Context path="/$webapp_name" docBase="/opt/artifactory/webapps/$webapp_name.war" processTlds="false">
+<Context path="/${RELATIVE_URL}" docBase="/opt/artifactory/webapps/${RELATIVE_URL}.war" processTlds="false">
     <Manager pathname="" />
     <Valve className="org.apache.catalina.valves.RemoteIpValve" protocolHeader="x-forwarded-proto"/>
 </Context>
 EOF
+mv /opt/artifactory/webapps/artifactory.war /opt/artifactory/webapps/${RELATIVE_URL}.war
+fi
+fi
+
+chown -R artifactory:artifactory /opt/artifactory
+chmod 750 -R /opt/artifactory/
 
 sudo -u artifactory sh -c "/opt/artifactory/bin/artifactory.sh"
