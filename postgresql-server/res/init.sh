eca5fac221bf0ac367cf4df2857592ba906e2733
Jon Herrmann <herrmann@interactive-instruments.de>
2016-04-22 13:09:56 +0200

    support volume sql scripts

--- postgresql-server/res/init.sh
+++ postgresql-server/res/init.sh
@@ -9,3 +9,7 @@ echo "checkpoint_segments=32" >> /var/lib/postgresql/data/postgresql.conf
 echo "checkpoint_completion_target=0.9" >> /var/lib/postgresql/data/postgresql.conf
 
 echo "host all  all    0.0.0.0/0  md5" >> /var/lib/postgresql/data/pg_hba.conf
+
+chown root:root /docker-entrypoint-initdb.d/*.sql
+chmod 550 /docker-entrypoint-initdb.d/*.sql
+ls -l /docker-entrypoint-initdb.d
